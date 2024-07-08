8be83bbe89b239f64ed32ecf4ebdb019756d8d78
pedro felipe <pedrofelipinho@gmail.com>
2024-07-08 10:01:12 -0300

    Alterando para o entity o método de listar

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco;
 
 internal class ScreenSoundContext : DbContext
 {
+    public DbSet<Artista> Artistas { get; set; }
+
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
