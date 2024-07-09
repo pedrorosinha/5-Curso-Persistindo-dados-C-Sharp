d7c4883fb02aa6a428a108ab37e8edf564d97fce
pedro felipe <pedrofelipinho@gmail.com>
2024-07-09 10:04:53 -0300

    Refatorando e levando os métodos para a classe pai DAL

--- ScreenSound/Banco/MusicaDAL.cs
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -3,44 +3,10 @@ using ScreenSound.Modelos;
 
 internal class MusicaDAL : DAL<Musica>
 {
-    private readonly ScreenSoundContext context;
-
-    public MusicaDAL(ScreenSoundContext context)
-    {
-        this.context = context;
-    }
-
-    public override IEnumerable<Musica> Listar()
-    {
-
-        return context.Musicas.ToList();
-
-    }
-
-    public override void Adicionar(Musica musica)
-    {
-        context.Musicas.Add(musica);
-        context.SaveChanges();
-
-    }
-
-    public override void Atualizar(Musica musica)
-    {
-        context.Musicas.Update(musica);
-        context.SaveChanges();
-
-    }
-
-    public override void Deletar(Musica musica)
-    {
-        context.Musicas.Remove(musica);
-        context.SaveChanges();
-
-    }
+    public MusicaDAL(ScreenSoundContext context) : base(context) { }
 
     public Musica? RecuperarPeloNome(string nome)
     {
         return context.Musicas.FirstOrDefault(a => a.Nome.Equals(nome));
-
     }
 }
