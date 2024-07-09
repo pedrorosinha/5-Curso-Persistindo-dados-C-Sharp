a88c223c7bd4a37cd9a2619f2ec89a8591b58471
pedro felipe <pedrofelipinho@gmail.com>
2024-07-09 11:02:09 -0300

    Refatorando os menus usando o Generics

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -3,23 +3,7 @@ using ScreenSound.Menus;
 using ScreenSound.Modelos;
 
 var context = new ScreenSoundContext();
-var artistaDAL = new ArtistaDAL(context);
-var musicaDAL = new MusicaDAL(context);
-
-var musicaNova = new Musica("Fuel") { Id = 2 };
-var musica2 = new Musica("One");
-var musica3 = new Musica("Enter Sandman");
-musicaDAL.Adicionar(musica3);
-//musicaDAL.Atualizar(musicaNova);
-//musicaDAL.Deletar(musicaNova);
-
-var listaDeMusicas = musicaDAL.Listar();
-foreach (var musica in listaDeMusicas)
-{
-    Console.WriteLine(musica);
-}
-
-return;
+var artistaDAL = new DAL<Artista>(context);
 
 Dictionary<int, Menu> opcoes = new();
 opcoes.Add(1, new MenuRegistrarArtista());
