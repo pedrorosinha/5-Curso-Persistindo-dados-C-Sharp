71aa3c5590aa071cf2ad47c991f9ec38bd8557db
pedro felipe <pedrofelipinho@gmail.com>
2024-07-10 15:12:42 -0300

    Criando a requisição PUT (atualizar)

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -44,4 +44,19 @@ app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
     return Results.NoContent();
 });
 
+app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
+{
+    var artistaAAtualizar = dal.RecuperarPor(a => a.Id == artista.Id);
+    if (artistaAAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+    artistaAAtualizar.Nome = artista.Nome;
+    artistaAAtualizar.Bio = artista.Bio;
+    artistaAAtualizar.FotoPerfil = artista.FotoPerfil;
+
+    dal.Atualizar(artistaAAtualizar);
+    return Results.Ok();
+});
+
 app.Run();
