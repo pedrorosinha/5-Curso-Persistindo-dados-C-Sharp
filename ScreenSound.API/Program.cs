4441cfa08fcfc0040fefa7d3374e21eab8744837
pedro felipe <pedrofelipinho@gmail.com>
2024-07-10 16:01:35 -0300

    Criando as requisições para a classe Musica

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
@@ -56,6 +57,52 @@ app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista art
     artistaAAtualizar.FotoPerfil = artista.FotoPerfil;
 
     dal.Atualizar(artistaAAtualizar);
+    return Results.Ok();
+});
+
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
+{
+    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(musica);
+
+});
+
+app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
+    var musica = dal.RecuperarPor(a => a.Id == id);
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(musica);
+    return Results.NoContent();
+
+});
+
+app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
+    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+    if (musicaAAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+    musicaAAtualizar.Nome = musica.Nome;
+    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+
+    dal.Atualizar(musicaAAtualizar);
     return Results.Ok();
 });
 
