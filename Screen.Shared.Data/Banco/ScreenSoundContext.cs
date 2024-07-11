b7ef8ae57a97acf40643d147002bca2d63679070
pedro felipe <pedrofelipinho@gmail.com>
2024-07-11 08:48:32 -0300

    Criando a entidade Genero e a migation para adicionar a tabela

--- Screen.Shared.Data/Banco/ScreenSoundContext.cs
+++ Screen.Shared.Data/Banco/ScreenSoundContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Screen.Shared.Modelos.Modelos;
 using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco;
@@ -9,6 +10,8 @@ public class ScreenSoundContext : DbContext
     public DbSet<Artista> Artistas { get; set; }
 
     public DbSet<Musica> Musicas { get; set; }
+
+    public DbSet<Genero> Generos { get; set; }
 
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
